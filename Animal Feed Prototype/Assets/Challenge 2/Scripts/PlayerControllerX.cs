f8ff1e88f85d2eb2f1612c5fb8b9311140c1b268
Jackson Luu <jacksonluu2006@hotmail.com>
2020-03-31 17:44:18 +1100

    added dog fetch game

---
+++ Animal Feed Prototype/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float prevSpawn = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float currTime = Time.time;
+
+            // Prevent player from spamming dogs
+            if (currTime - prevSpawn >= 1)
+            {
+                prevSpawn = currTime;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+    }
+}
